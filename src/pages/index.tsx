23e20791d3969e0f05f6afbd62f310def07964b1
qiushaorong <345239925@qq.com>
2020-11-06 09:32:35 +0800

    similar async data fetched

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './index.less';
 import {LineChart} from '../components/chatrs'
 
 
 // s数据结构如下
 
-const RvehicleChartOpts = {
-  x_data: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
-  seriesData: [
-    {
-      name: 'Rvehicle',
-      data: [100, 620, 431 ,354, 382, 123, 132, 333]
-    },
-    {
-      name: 'Face',
-      data: [322, 820, 131 ,987, 768, 874, 654, 356]
-    }
-  ]
-}
 
 const RealTimeChartOpts = {
   x_data: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
@@ -28,14 +15,69 @@ const RealTimeChartOpts = {
     }
   ]
 }
+const RvehicleChartOpts = {
+  x_data: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+  seriesData: [
+    {
+      name: 'Rvehicle',
+      data: []
+    },
+    {
+      name: 'Face',
+      data: []
+    }
+  ]
+}
+
+function getRandomData() {
+  let data = []
+  for (let i = 0; i<7; i++) {
+    data.push(Math.floor(Math.random() * 100))
+  }
+  return data
+}
+
+function getSimRvehicleData () {
+  return {
+    x_data: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+    seriesData: [
+      {
+        name: 'Rvehicle',
+        data: getRandomData()
+      },
+      {
+        name: 'Face',
+        data: getRandomData()
+      }
+    ]
+  }
+}
+
 
 export default () => {
+
+  const [rvehicleOpts, setRvehicleOpts] = useState(RvehicleChartOpts)
+  useEffect(() => {
+    setTimeout(() => {
+      const data:any = getSimRvehicleData()
+      setRvehicleOpts(data)
+    }, 1000)
+
+    let rvehicleTimer = setInterval(() => {
+      const data:any = getSimRvehicleData()
+      setRvehicleOpts(data)
+    }, 3000)
+    return () => {
+       clearInterval(rvehicleTimer)
+    }
+  }, [])
+
   return (
     <div>
       charts demo
       <div className="infos-left">
         <LineChart 
-          option={RvehicleChartOpts}
+          option={rvehicleOpts}
         />
          <LineChart 
           option={RealTimeChartOpts}
